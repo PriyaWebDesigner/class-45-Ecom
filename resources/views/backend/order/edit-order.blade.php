035828affa877a10fdd0d1f51ef54532db0c6525
PriyaWebDesigner <naskartanu@111gmail.com>
2024-12-29 12:29:54 +0530

    Order process is running

--- resources/views/backend/order/edit-order.blade.php
+++ resources/views/backend/order/edit-order.blade.php
@@ -16,45 +16,56 @@
                     <!-- /.card-header -->
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Invoice Number</label>
-                                    <input type="text" name="invoiceId" value="{{$order->invoiceId}}" class="form-control" readonly>
+                            <div class="col-6">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Invoice Number</label>
+                                        <input type="text" name="invoiceId" value="{{$order->invoiceId}}" class="form-control" readonly>
+                                    </div>
+                                </div>
+    
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Customer Name</label>
+                                        <input type="text" name="c_name" value="{{$order->c_name}}" class="form-control" required>
+                                    </div>
+                                </div>
+    
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Customer Phone</label>
+                                        <input type="text" name="c_phone" value="{{$order->c_phone}}" class="form-control" required>
+                                    </div>
+                                </div>
+    
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Customer Address</label>
+                                        <textarea name="address" class="form-control" required>{{$order->address}}</textarea>
+                                    </div>
+                                </div>
+    
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Delivery Charge</label>
+                                        <input type="text" name="area" value="{{$order->area}}" class="form-control" required>
+                                    </div>
+                                </div>
+                                
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label>Select Courier</label>
+                                        <select name="courier_name" class="form-control">
+                                            <option value="steadfast">Steadfast</option>
+                                            <option value="redx">REDX</option>
+                                            <option value="other">Other</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Customer Name</label>
-                                    <input type="text" name="c_name" value="{{$order->c_name}}" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Customer Phone</label>
-                                    <input type="text" name="c_phone" value="{{$order->c_phone}}" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Customer Address</label>
-                                    <textarea name="address" class="form-control" required>{{$order->address}}</textarea>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Delivery Charge</label>
-                                    <input type="text" name="area" value="{{$order->area}}" class="form-control" required>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label>Select Courier</label>
-                                    <select name="courier_name" class="form-control">
-                                        <option value="steadfast">Steadfast</option>
-                                        <option value="redx">REDX</option>
-                                        <option value="other">Other</option>
-                                    </select>
-                                </div>
+
+                            <div class="col-6">
+                                
                             </div>
     
                             <div class="col-md-12">
